8d5d0fafcd1614050b503b017acdfe84f2b7ef3c
mjoporto <mjoporto1@gmail.com>
2025-05-12 08:48:19 -0500

    Day 2first commit

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,20 @@
 len("Hello")
+#subscripting
+print("Hello"[0])
+
+#String
+print("123" +"345")
+
+#Integer=whole number
+print(123+345)
+
+#Large integers
+print(123_456_789)
+
+#float = floating point number
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
+
