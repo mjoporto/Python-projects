294856c32502d78c8e33c3fd4ae5df2ba17d8b0c
mjoporto <mjoporto1@gmail.com>
2025-05-26 13:27:25 +0100

    completed day 10

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -5,3 +5,26 @@ def format_name(f_name, l_name):
 
 
 print(format_name("AnGEla", "YU"))
+
+#leap year test
+# def is_leap_year(year):
+    # Write your code here.
+    # Don't change the function name.
+    # leap_year = ""
+    # divisible_by_4 = year % 4
+    # if divisible_by_4 == 0:
+    #     divisible_by_100 = year % 100
+    #     if divisible_by_100 == 0:
+    #         divisible_by_400 = year % 400
+    #         if divisible_by_400 == 0:
+    #             leap_year = True
+    #             return leap_year
+    #         else:
+    #             leap_year = False
+    #             return leap_year
+    #     else:
+    #         leap_year = True
+    #         return leap_year
+    # else:
+    #     leap_year = False
+    #     return leap_year
